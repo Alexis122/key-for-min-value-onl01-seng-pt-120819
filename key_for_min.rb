fe9a8d7dd488821cc854db8ff0622b38109cb5a0
Alexis Priore <alexispriore@gmail.com>
2019-12-22 20:36:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+birthday_kids = {
+    "blake" => 10, 
+    "ashley" => 50, 
+    "adam" => 17
+}
+ 
+birthday_kids.collect do |kids_name, age|
+    age
+  end
 end
+
